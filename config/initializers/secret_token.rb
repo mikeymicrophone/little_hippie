aa2992f3905db6626cdd9a1abb34f61345bc8b7e
Mike Schwab <mike.schwab@gmail.com>
2012-11-04 21:35:05 -0500

    freshly generated Rails 3.2 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LittleHippie::Application.config.secret_token = 'a5623cdd7387b5dadf40c72eacd90d887774de3862cfbb9eea019b6c08c1f074b92004782b24e0c4318e03ab4bf0f11094f101e1c4334c7e5a1b0561205b5717'
