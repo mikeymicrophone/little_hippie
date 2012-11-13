b4cc7b1fab1e68f526f81c33a82ef5abb7e582d4
Mike Schwab <mike.schwab@gmail.com>
2012-11-13 15:02:07 -0500

    images reinstated on home page

--- app/models/body_style.rb
+++ app/models/body_style.rb
@@ -2,5 +2,5 @@ class BodyStyle < ActiveRecord::Base
   has_many :products
   attr_accessible :code, :name
   acts_as_list
-  default_scope :order => :position
+  default_scope :order => 'body_styles.position'
 end
