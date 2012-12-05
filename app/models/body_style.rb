f1574cc4d771e630cbc56df09bd1208fd35f5e4d
Mike Schwab <mike.schwab@gmail.com>
2012-12-04 22:24:39 -0500

    display sizes on body style show page and vice versa

--- app/models/body_style.rb
+++ app/models/body_style.rb
@@ -1,5 +1,7 @@
 class BodyStyle < ActiveRecord::Base
   has_many :products
+  has_many :body_style_sizes
+  has_many :sizes, :through => :body_style_sizes
   attr_accessible :code, :name, :base_price, :image
   mount_uploader :image, ProductImageUploader
   acts_as_list
--- app/models/size.rb
+++ app/models/size.rb
@@ -1,5 +1,7 @@
 class Size < ActiveRecord::Base
   has_many :inventories
+  has_many :body_style_sizes
+  has_many :body_styles, :through => :body_style_sizes
   attr_accessible :code, :name
   acts_as_list
   scope :ordered, {:order => 'sizes.position'}
