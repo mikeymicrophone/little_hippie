b90f16223ceb9f28cf92eab97b557ee83ee651ad
Mike Schwab <mike.schwab@gmail.com>
2013-01-03 00:41:33 -0500

    cc can be exchanged for token

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,8 @@ class Cart < ActiveRecord::Base
   belongs_to :customer
   has_many :items
   attr_accessible :status, :customer
+  
+  def subtotal
+    items.inject(0) { |sum, item| sum + item.product.dollar_price }
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ActiveRecord::Base
   belongs_to :cart
   belongs_to :product_color
+  has_one :product, :through => :product_color
   belongs_to :size
   attr_accessible :product_color_id, :size_id
 end
