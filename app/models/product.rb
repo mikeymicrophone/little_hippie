3505d059ceaf14c51eebbffe092f6e72aabd2642
Mike Schwab <mike.schwab@gmail.com>
2013-01-03 15:23:28 -0500

    removed callback creating product_sizes

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,7 +6,6 @@ class Product < ActiveRecord::Base
   attr_accessible :design_id, :body_style_id, :price, :active, :code, :copy
   scope :active, {:conditions => {:active => true}}
   before_create :use_base_price, :generate_code, :default_to_active
-  after_create :create_inventory_in_body_style_sizes
   acts_as_list
   scope :ordered, :order => :position
   
@@ -40,11 +39,5 @@ class Product < ActiveRecord::Base
   
   def default_to_active
     self.active = true
-  end
-  
-  def create_inventory_in_body_style_sizes
-    body_style.sizes.each do |size|
-      product_sizes.create :size => size
-    end
   end
 end
