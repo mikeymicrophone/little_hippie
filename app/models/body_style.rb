93ad5359f3e8d7d0fb38ebd82df7bf6fffac2ffe
Mike Schwab <mike.schwab@gmail.com>
2013-01-20 20:34:58 -0500

    sizes will display in order on product_colors page

--- app/models/body_style.rb
+++ app/models/body_style.rb
@@ -1,7 +1,7 @@
 class BodyStyle < ActiveRecord::Base
   has_many :products
-  has_many :body_style_sizes
-  has_many :sizes, :through => :body_style_sizes
+  has_many :body_style_sizes, :order => :position
+  has_many :sizes, :through => :body_style_sizes, :order => 'body_style_sizes.position'
   attr_accessible :code, :name, :base_price, :image
   mount_uploader :image, ProductImageUploader
   acts_as_list
