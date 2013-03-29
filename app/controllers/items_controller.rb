212d804590333750de00a77e0a37616aa0a71010
Mike Schwab <mike.schwab@gmail.com>
2013-03-29 16:32:20 -0400

    extra countries disabled to speed up cart

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -84,6 +84,7 @@ class ItemsController < ApplicationController
     @item.destroy
 
     respond_to do |format|
+      format.js   { render :nothing => true }
       format.html { redirect_to items_url }
       format.json { head :no_content }
     end
