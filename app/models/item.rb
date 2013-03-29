3709098aabdffc247ffa517943c91d50edb2990a
Mike Schwab <mike.schwab@gmail.com>
2013-03-29 16:46:27 -0400

    color images in cart

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,15 @@
 class Item < ActiveRecord::Base
   belongs_to :cart
   belongs_to :product_color
+  has_one :color, :through => :product_color
   has_one :product, :through => :product_color
   belongs_to :size
   attr_accessible :product_color_id, :size_id, :quantity, :gift_wrap
   validates_presence_of :cart_id, :product_color_id, :size_id
   
   before_create :set_default_quantity
+  
+  delegate :css_hex_code, :to => :color
   
   def name
     "#{product_color.name} in #{size.name}"
