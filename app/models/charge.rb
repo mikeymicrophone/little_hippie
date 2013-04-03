45717671d49c295532b6d14bedf9d3d8aebccd5c
Mike Schwab <mike.schwab@gmail.com>
2013-04-03 08:17:56 -0400

    charges shown correctly in admin panel

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -2,4 +2,8 @@ class Charge < ActiveRecord::Base
   belongs_to :cart
   attr_accessible :cart_id, :amount, :token
   scope :complete, where(:result => 'complete')
+  
+  def dollar_amount
+    amount / 100.0
+  end
 end
