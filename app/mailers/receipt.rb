c3aa756519cb07729e725c9f5f7d93da6f4b21e1
Mike Schwab <mike.schwab@gmail.com>
2013-04-03 15:47:23 -0400

    removed receipts@littlehippie.com bcc on receipts

--- app/mailers/receipt.rb
+++ app/mailers/receipt.rb
@@ -1,5 +1,5 @@
 class Receipt < ActionMailer::Base
-  default from: "orders@littlehippie.com", bcc: 'receipts@littlehippie.com'
+  default from: "orders@littlehippie.com"#, bcc: 'receipts@littlehippie.com'
   
   def purchase_receipt charge, stripe_customer
     cart = charge.cart
