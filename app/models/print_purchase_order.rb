1474b64adca67af64f3866ee689afd0ab90a3ad4
Mike Schwab <mike.schwab@gmail.com>
2013-04-12 19:15:12 -0400

    work on print purchase order form

--- app/models/print_purchase_order.rb
+++ app/models/print_purchase_order.rb
@@ -2,5 +2,6 @@ class PrintPurchaseOrder < ActiveRecord::Base
   belongs_to :supplier
   belongs_to :billing_address
   belongs_to :garment_purchase_order
-  # attr_accessible :title, :body
+  has_many :quantities
+  attr_accessible :supplier_id, :billing_address_id, :garment_purchase_order_id
 end
