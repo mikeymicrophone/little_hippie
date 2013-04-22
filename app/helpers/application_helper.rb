89107b10e219bac3208cb8a3310f2e16067a2c8a
Mike Schwab <mike.schwab@gmail.com>
2013-04-22 19:47:28 -0400

    google analytics on customer layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,6 +20,22 @@ module ApplicationHelper
     end
   end
   
+  def google_analytics
+    script = <<-GOOGLE_ANALYTICS
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', '#{ENV['GOOGLE_ANALYTICS_CODE']}', '#{request.host}');
+      ga('send', 'pageview');
+
+    </script>
+    GOOGLE_ANALYTICS
+    script.html_safe
+  end
+  
   def clearboth
     content_tag(:div, :class => 'clearboth') {}
   end
