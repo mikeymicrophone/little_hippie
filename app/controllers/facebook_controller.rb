fb5756db968fc1a0e8b10b315d63db813ab6d55c
Mike Schwab <mike.schwab@gmail.com>
2013-04-22 21:19:29 -0400

    beta works with Facebook

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -4,11 +4,15 @@ class FacebookController < ApplicationController
     
     if (@customer = Customer.find_by_email current_facebook_user.email)
       @customer.update_attribute(:facebook_id, current_facebook_user.id) if @customer.facebook_id.blank?
+      sign_in @customer
+      redirect_to root_url
     else
-      @customer = Customer.create :email => current_facebook_user.email, :facebook_id => current_facebook_user.id
+      Invitation.create :email => current_facebook_user.email, :name => current_facebook_user.name, :note => 'Used Facebook Connect'
+      render :action => 'beta_account_requested', :layout => 'customer'
+      # suspended this code during beta period
+      # @customer = Customer.create :email => current_facebook_user.email, :facebook_id => current_facebook_user.id
+      # sign_in @customer
+      # redirect_to root_url
     end
-    
-    sign_in @customer
-    redirect_to root_url
   end
 end
