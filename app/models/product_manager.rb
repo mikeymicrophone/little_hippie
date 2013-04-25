9167738b8ff3ef47d8c564bff3508f90b2200104
Mike Schwab <mike.schwab@gmail.com>
2013-04-24 21:09:46 -0400

    product managers cannot register themselves

--- app/models/product_manager.rb
+++ app/models/product_manager.rb
@@ -2,7 +2,7 @@ class ProductManager < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   # Setup accessible (or protected) attributes for your model
