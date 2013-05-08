90dd57fd1a33d16cfbc8c3972500f5f2fdd90601
Mike Schwab <mike.schwab@gmail.com>
2013-05-08 15:20:15 -0400

    added a from address so order notifier can be mailed

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,6 +2,6 @@ class OrderMailer < ActionMailer::Base
   def notify_retailer cart
     @cart = cart
     
-    mail :to => "info@littlehippie.com", :subject => "New Order"
+    mail :to => "taylorswope@littlehippie.com", :subject => "New Order", :from => "sales@littlehippie.com"
   end
 end
