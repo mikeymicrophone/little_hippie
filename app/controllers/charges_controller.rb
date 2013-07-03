f66a12db9860754300fcccb58f62f6f87c9ab649
Mike Schwab <mike.schwab@gmail.com>
2013-07-03 13:40:22 -0400

    coupons respect start and expire dates

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -63,6 +63,7 @@ class ChargesController < ApplicationController
     end
     
     @coupon = Coupon.find_by_code(params[:coupon_code])
+    @coupon = nil unless @coupon.valid_on_this_date?
     if @coupon
       @cart.coupon = @coupon
     end
--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -2,4 +2,18 @@ class Coupon < ActiveRecord::Base
   has_many :charges
   
   attr_accessible :amount, :code, :expiration_date, :lower_limit, :name, :percentage, :upper_limit, :valid_date
+  
+  def valid_on_this_date?
+    if valid_date.present?
+      if expiration_date.present?
+        (valid_date <= Time.now) && (Time.now <= expiration_date)
+      else
+        valid_date <= Time.now
+      end
+    elsif expiration_date.present?
+      Time.now <= expiration_date
+    else
+      true
+    end
+  end
 end
