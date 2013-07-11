769a4beabaab9ecd3464631a5525213fd9b80685
Mike Schwab <mike.schwab@gmail.com>
2013-07-11 17:42:09 -0400

    ® removed from invitation subject line

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,7 +1,7 @@
 class InvitationMailer < ActionMailer::Base
   def beta_invitation_approved invitation
     @invitation = invitation
-    mail :to => invitation.email, :subject => "Your invitation to Little Hippie®'s private beta has been approved", :from => 'beta@littlehippie.com'
+    mail :to => invitation.email, :subject => "Your invitation to Little Hippie's private beta has been approved", :from => 'beta@littlehippie.com'
   end
   
   def beta_invitation_requested invitation
