88c9f0c5d0912604d07d9b666a31cc07d953f906
Mike Schwab <mike.schwab@gmail.com>
2013-07-22 14:55:55 -0400

    try using gmail to send email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ LittleHippie::Application.configure do
   config.action_mailer.smtp_settings = {
     :user_name => ENV['EMAIL_ADDRESS'],
     :password => ENV['EMAIL_PASSWORD'],
-    :address => 'smtp.hostcentric.com',
+    :address => 'smtp.gmail.com',
     :authentication => :plain,
     :port => 587,
     :enable_starttls_auto => false
