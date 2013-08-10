448b944fb4db846eb82762e7c1aa58787fcee1c8
Mike Schwab <mike.schwab@gmail.com>
2013-08-09 21:49:45 -0400

    seems like this should be the way to use js with fb login

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -5,14 +5,12 @@ class FacebookController < ApplicationController
     if (@customer = Customer.find_by_email current_facebook_user.email)
       @customer.update_attribute(:facebook_id, current_facebook_user.id) if @customer.facebook_id.blank?
       sign_in @customer
-      redirect_to root_url
     else
       # Invitation.create :email => current_facebook_user.email, :name => current_facebook_user.name, :note => 'Used Facebook Connect'
       # render :action => 'beta_account_requested', :layout => 'customer'
       # above is the 'private beta' logic. below is the 'public beta' logic.
       @customer = Customer.create :email => current_facebook_user.email, :facebook_id => current_facebook_user.id
       sign_in @customer
-      redirect_to root_url
     end
   end
 end
