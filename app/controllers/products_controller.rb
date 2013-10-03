005cdbb34df294c4d2f5e91d5ac2d996873e5012
Mike Schwab <mike.schwab@gmail.com>
2013-10-03 11:14:02 -0700

    only active products shown in search results

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,7 +19,7 @@ class ProductsController < ApplicationController
     
     @products = Product.search params[:query]
     @colors = Color.search params[:query]
-    @product_colors = (@products.map(&:product_colors).flatten + @colors.map(&:product_colors).flatten).uniq
+    @product_colors = (@products.map { |p| p.product_colors.active_product }.flatten + @colors.map { |c| c.product_colors.active_product }.flatten).uniq
     
     if price
       @products_below_price = Product.where('price < ?', price * 101)
