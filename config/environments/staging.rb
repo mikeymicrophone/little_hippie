38e17e0b002cadd238ff8dc06e581973b3a5c551
Mike Schwab <mike.schwab@gmail.com>
2013-10-08 19:01:00 -0700

    let's see staging delivery errors

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -49,7 +49,7 @@ LittleHippie::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
