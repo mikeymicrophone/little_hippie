e015e089f0bd70894f1f84b5bc1374b4468f00ab
Mike Schwab <mike.schwab@gmail.com>
2013-10-18 20:10:23 -0700

    likes count shown on home page

--- app/models/bulletin.rb
+++ app/models/bulletin.rb
@@ -1,7 +1,7 @@
 class Bulletin < ActiveRecord::Base
   has_many :bulletin_pairings, :dependent => :destroy
   has_many :content_pages, :through => :bulletin_pairings
-  has_many :likes, :as => :favorite
+  has_many :likes, :as => :favorite, :dependent => :destroy
   attr_accessible :active, :content, :title, :teaser, :created_at, :facebook_post_id, :facebook_image_url
   scope :active, where(:active => true)
   scope :alphabetical, order(:title)
