d36e174fe3b844df85926760722a544f5d0da58f
Mike Schwab <mike.schwab@gmail.com>
2013-10-18 20:13:10 -0700

    typo

--- app/controllers/bulletins_controller.rb
+++ app/controllers/bulletins_controller.rb
@@ -23,7 +23,7 @@ class BulletinsController < ApplicationController
         BulletinPairing.create :bulletin => bulletin, :content_page => home
         likes = post.likes["data"]
         likes.each do |like_data|
-          l = bulletin.likes.new :facebook_like_id => like_data["id"], :facebook_user_name => like_data["name"]
+          l = bulletin.likes.new :facebook_user_id => like_data["id"], :facebook_user_name => like_data["name"]
           if customer = Customer.find_by_facebook_id(like_data["id"])
             l.customer_id = customer.id
           end
