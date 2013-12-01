70ae02183ce318bd129e64433162be88e41962f1
Mike Schwab <mike.schwab@gmail.com>
2013-11-30 16:09:54 -0800

    bulletin fb id validation should allow nil

--- app/models/bulletin.rb
+++ app/models/bulletin.rb
@@ -6,6 +6,6 @@ class Bulletin < ActiveRecord::Base
   scope :active, where(:active => true)
   scope :alphabetical, order(:title)
   alias_attribute :name, :title
-  validates_uniqueness_of :content, :facebook_post_id
+  validates_uniqueness_of :content, :facebook_post_id, :allow_nil => true
   validates_presence_of :content
 end
