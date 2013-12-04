7b11ec1933172946ef1ba7962959c05925fd28fa
Mike Schwab <mike.schwab@gmail.com>
2013-12-03 20:49:47 -0800

    banners added to list ordering when added to gallery

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -6,9 +6,21 @@ class Banner < ActiveRecord::Base
   scope :ordered, order(:gallery_position)
   acts_as_list :column => :gallery_position
   
+  after_save :manage_gallery_ordering
+  
   mount_uploader :image, BannerUploader
   
   define_index do
     indexes name
+  end
+  
+  def manage_gallery_ordering
+    if active_in_gallery_changed?
+      if active_in_gallery?
+        insert_at
+      else
+        remove_from_list
+      end
+    end
   end
 end
