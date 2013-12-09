8ae371436dd8798725a693cc096819d5cecc8637
Mike Schwab <mike.schwab@gmail.com>
2013-12-08 19:16:55 -0800

    billing address removed from order mailer

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -4,11 +4,11 @@ class OrderMailer < ActionMailer::Base
   def notify_retailer cart_id, stripe_customer
     @cart = Cart.find cart_id
     
-    @billing_address = stripe_customer.active_card.name
-    @billing_address += "<br>#{stripe_customer.active_card.address_line1}"
-    @billing_address += "<br>#{stripe_customer.active_card.address_line2}" if stripe_customer.active_card.address_line2.present?
-    @billing_address += "<br>#{stripe_customer.active_card.address_city} #{stripe_customer.active_card.address_zip}"
-    @billing_address += "<br>**** **** **** #{stripe_customer.active_card.last4}"
+    # @billing_address = stripe_customer.active_card.name
+    # @billing_address += "<br>#{stripe_customer.active_card.address_line1}"
+    # @billing_address += "<br>#{stripe_customer.active_card.address_line2}" if stripe_customer.active_card.address_line2.present?
+    # @billing_address += "<br>#{stripe_customer.active_card.address_city} #{stripe_customer.active_card.address_zip}"
+    # @billing_address += "<br>**** **** **** #{stripe_customer.active_card.last4}"
     
     mail :to => [ENV['PRIMARY_RETAILER_RECEIPT_EMAIL'], ENV['BACKUP_RETAILER_RECEIPT_EMAIL']], :subject => "New Order in #{Rails.env}", :from => "little.hippie.messenger@gmail.com"
   end
