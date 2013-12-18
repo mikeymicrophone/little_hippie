2125c8fd1d201c0c04610813a6d6486a4c3af2ba
Mike Schwab <mike.schwab@gmail.com>
2013-12-17 23:07:37 -0500

    added Resque before_fork and after_fork connection management

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,10 @@
 uri = ENV['REDISCLOUD_URL']
 Resque.redis = uri #Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
+
+Resque.before_fork do
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+end
+
+Resque.after_fork do
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
