a7a2e1939574d622526e70416a4094806ed53eba
Mike Schwab <mike.schwab@gmail.com>
2013-12-17 23:31:50 -0500

    use stripe_customer as a hash within mailers

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -5,11 +5,11 @@ class OrderMailer < ActionMailer::Base
     @cart = Cart.find cart_id
     @charge = @cart.charges.last
     
-    @billing_address = stripe_customer.active_card.name
-    @billing_address += "<br>#{stripe_customer.active_card.address_line1}"
-    @billing_address += "<br>#{stripe_customer.active_card.address_line2}" if stripe_customer.active_card.address_line2.present?
-    @billing_address += "<br>#{stripe_customer.active_card.address_city} #{stripe_customer.active_card.address_zip}"
-    @billing_address += "<br>**** **** **** #{stripe_customer.active_card.last4}"
+    @billing_address = stripe_customer['active_card']['name']
+    @billing_address += "<br>#{stripe_customer['active_card']['address_line1']}"
+    @billing_address += "<br>#{stripe_customer['active_card']['address_line2']}" if stripe_customer.active_card.address_line2.present?
+    @billing_address += "<br>#{stripe_customer['active_card']['address_city']} #{stripe_customer['active_card']['address_zip']}"
+    @billing_address += "<br>**** **** **** #{stripe_customer['active_card']['last4']}"
     
     mail :to => [ENV['PRIMARY_RETAILER_RECEIPT_EMAIL'], ENV['BACKUP_RETAILER_RECEIPT_EMAIL']], :subject => "New Order in #{Rails.env}", :from => "little.hippie.messenger@gmail.com"
   end
