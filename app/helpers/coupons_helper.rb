fbe88eb21e3b331e1c169c0f5aaf0bf73f03b8dc
Mike Schwab <mike.schwab@gmail.com>
2013-12-19 16:13:21 -0500

    incentive coupon can be loaded with special offers in cart

--- app/helpers/coupons_helper.rb
+++ app/helpers/coupons_helper.rb
@@ -1,2 +1,5 @@
 module CouponsHelper
+  def incentive_coupon
+    Coupon.find_by_code YAML::load(File.open(File.join(Rails.root, 'config', 'incentive_coupon.yml'))).andand['coupon_code'] rescue nil
+  end
 end
--- app/helpers/product_colors_helper.rb
+++ app/helpers/product_colors_helper.rb
@@ -1,10 +1,10 @@
 module ProductColorsHelper
-  def product_display_box product_color
+  def product_display_box product_color, coupon_id = nil
     div_for product_color.product, :class => 'stack' do
       like_heart_for(product_color.product) +
-	    (product_color.product.primary_image.present? ? link_to(image_tag(product_color.product.primary_image(:product_box), :style => "background-color:##{product_color.color.css_hex_code}"), detail_product_path(product_color.product)) : '') +
+	    (product_color.product.primary_image.present? ? link_to(image_tag(product_color.product.primary_image(:product_box), :style => "background-color:##{product_color.color.css_hex_code}"), detail_product_path(product_color.product, :coupon_id => coupon_id)) : '') +
 	    div_for(product_color.product, :gray_label_for, :class => 'gray_label') do
-	      link_to detail_product_path(product_color.product) do
+	      link_to detail_product_path(product_color.product, :coupon_id => coupon_id) do
   	      (product_color.product.design.name + '<br>' + product_color.product.body_style.name).html_safe
   	    end
 	    end
