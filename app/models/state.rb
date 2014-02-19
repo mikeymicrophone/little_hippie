62cb9f1976a65271c405e69fb909d388cfabea7d
Mike Schwab <mike.schwab@gmail.com>
2014-02-18 22:12:25 -0500

    tax charged if state changed to CT

--- app/models/state.rb
+++ app/models/state.rb
@@ -2,4 +2,8 @@ class State < ActiveRecord::Base
   belongs_to :country
   attr_accessible :country_id, :name, :iso, :id
   scope :alphabetical, {:order => 'name'}
+  
+  def self.connecticut
+    find_by_name('Connecticut')
+  end
 end
