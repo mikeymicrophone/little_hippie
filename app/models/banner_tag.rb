b3c3c7600a2a244d79b2e5839d95f8dda447d1cc
Mike Schwab <mike.schwab@gmail.com>
2014-02-25 21:01:35 -0500

    banner tags can be set with product codes

--- app/models/banner_tag.rb
+++ app/models/banner_tag.rb
@@ -1,10 +1,12 @@
 class BannerTag < ActiveRecord::Base
   belongs_to :banner
   belongs_to :tag, :polymorphic => true
-  attr_accessible :active, :tag_id, :tag_type, :banner_id, :tag
+  attr_accessible :active, :tag_id, :tag_type, :banner_id, :tag, :product_code
   validates_uniqueness_of :tag_id, :scope => [:banner_id, :tag_type]
   validates_presence_of :banner_id, :tag_id, :tag_type
+  attr_accessor :product_code
   
+  before_validation :recognize_product_code
   after_create :bubble_up
   
   def name
@@ -22,6 +24,12 @@ class BannerTag < ActiveRecord::Base
     when 'Garment'
       banner.banner_tags.create :tag => tag.product_color
       banner.banner_tags.create :tag => tag.size
+    end
+  end
+  
+  def recognize_product_code
+    if product_code.present?
+      self.tag = Product.find_by_code product_code
     end
   end
 end
