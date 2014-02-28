cc3315b06fd388f98e96fcd911f65e525f5d925a
Mike Schwab <mike.schwab@gmail.com>
2014-02-28 00:53:09 -0500

    more pages shown

---
+++ config/initializers/kaminari.rb
@@ -0,0 +1,10 @@
+Kaminari.configure do |config|
+  # config.default_per_page = 25
+  # config.max_per_page = nil
+  config.window = 40
+  # config.outer_window = 0
+  config.left = 10
+  config.right = 10
+  # config.page_method_name = :page
+  # config.param_name = :page
+end
