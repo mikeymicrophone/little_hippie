a26940103194bac987d6506c9b5d4830f77deeae
Mike Schwab <mike.schwab@gmail.com>
2014-05-29 04:27:10 -0400

    inventory is updated via a background job

---
+++ app/jobs/inventory_update.rb
@@ -0,0 +1,50 @@
+class InventoryUpdate
+  def self.queue
+    :mailer
+  end
+  
+  def self.perform filename
+    csv = CSV.open(File.join(Rails.root, 'tmp', 'current_inventory.csv'), 'wb')
+    csv << [
+      'Code',
+      'Design',
+      'BodyStyle',
+      'Color',
+      'Size',
+      'Quantity'
+    ]
+    
+    inventory = Nokogiri::XML(File.open(filename))
+    inventory.xpath('//Product').each do |product_xml|
+      full_og_code = product_xml.xpath('SKU').first.content
+      product_color_code = full_og_code[/\d+/]
+      product_color = ProductColor.find_by_og_code product_color_code
+      next unless product_color
+      full_og_code =~ /\-(.*)/
+      size = Size.translation $1
+      quantity = product_xml.xpath('Quantity').first.content
+      
+      if size
+        garment = product_color.garments.of_size(size.id).of_color(product_color.color_id).first
+        if garment
+          Rails.logger.info "updating inventory of #{garment.name} to #{quantity}"
+          garment.set_inventory quantity
+        
+          csv << [
+            garment.product_color.og_code,
+            garment.design.name,
+            garment.body_style.name,
+            garment.color.name,
+            garment.size.name,
+            quantity
+          ]
+        else
+          Rails.logger.info "garment not found: #{product_xml.xpath('ProductName').first.content}"
+        end
+      end
+    end
+    csv.close
+    
+    ReportMailer.inventory_report(ENV['INVENTORY_REPORT_RECIPIENT_EMAIL'], File.join(Rails.root, 'tmp', 'current_inventory.csv'))
+  end
+end
--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -3,4 +3,9 @@ class ReportMailer < ActionMailer::Base
     attachments[filename] = File.read(File.join(Rails.root, 'tmp', filename))
     mail to: email, subject: 'Little Hippie Sales Report'
   end
+  
+  def inventory_report email, filename
+    attachments[filename] = File.read(File.join(Rails.root, 'tmp', filename))
+    mail to: email, subject: 'Little Hippie Inventory Report'    
+  end
 end
