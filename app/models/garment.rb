a5195df09ef3caa985cd7fd48cc3e2c7faecf9e1
Mike Schwab <mike.schwab@gmail.com>
2014-05-29 14:07:51 -0400

    update logic for deactivating out-of-stock products

--- app/models/garment.rb
+++ app/models/garment.rb
@@ -58,7 +58,7 @@ class Garment < ActiveRecord::Base
     inventory.andand.update_attribute :current_amount, inventory.andand.current_amount.andand.-(quantity)
     
     unless stashed?
-      if inventory.current_amount == 0
+      if inventory.current_amount <= 0
         other_sizes_and_colors = product.garments
         other_sizes_and_colors.reject! { |garment| garment.inventory.andand.current_amount <= 0 }
         if other_sizes_and_colors.length == 0
