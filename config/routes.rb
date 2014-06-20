c50bdfac7ba7b9d48604455dc8678b8a2da81d6a
Mike Schwab <mike.schwab@gmail.com>
2014-06-20 14:20:08 -0400

    edited route name

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ LittleHippie::Application.routes.draw do
   resources :wholesale_orders do
     collection do
       get :order
-      get 'body_style.:body_style_id' => :body_style, :as => 'body_style_wholesale_orders'
+      get 'body_style.:body_style_id' => :body_style, :as => 'body_style'
     end
   end
 
