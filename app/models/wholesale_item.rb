c6e46a4dd4c8584d5406ca61491cd7ff32447434
Mike Schwab <mike.schwab@gmail.com>
2014-07-13 21:09:14 -0400

    garments displayed on wholesale item index

--- app/models/wholesale_item.rb
+++ app/models/wholesale_item.rb
@@ -1,3 +1,6 @@
 class WholesaleItem < ActiveRecord::Base
   attr_accessible :garment_id, :quantity, :wholesale_order_id
+  validates_presence_of :garment_id, :wholesale_order_id
+  belongs_to :garment
+  belongs_to :wholesale_order
 end
