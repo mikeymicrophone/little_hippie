1b45ef387ff66ed18be08c8596a0c72d72a468e1
Mike Schwab <mike.schwab@gmail.com>
2014-12-01 19:45:13 -0500

    mailing list csv export

--- app/models/mailing_list_registration.rb
+++ app/models/mailing_list_registration.rb
@@ -7,4 +7,13 @@ class MailingListRegistration < ActiveRecord::Base
   def downcase_email
     self.email = email.downcase
   end
+  
+  def self.to_csv(options = {})
+    CSV.generate(options) do |csv|
+      csv << column_names
+      all.each do |product|
+        csv << product.attributes.values_at(*column_names)
+      end
+    end
+  end
 end
