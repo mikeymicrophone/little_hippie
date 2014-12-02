21ec2a7254d51197284121cb3f88845d5c3611e1
Mike Schwab <mike.schwab@gmail.com>
2014-12-01 19:44:41 -0500

    inventory update job does not attempt csv creation

--- app/jobs/inventory_update.rb
+++ app/jobs/inventory_update.rb
@@ -4,15 +4,15 @@ class InventoryUpdate
   end
   
   def self.perform filename
-    csv = CSV.open(File.join(Rails.root, 'tmp', 'current_inventory.csv'), 'wb')
-    csv << [
-      'Code',
-      'Design',
-      'BodyStyle',
-      'Color',
-      'Size',
-      'Quantity'
-    ]
+    # csv = CSV.open(File.join(Rails.root, 'tmp', 'current_inventory.csv'), 'wb')
+    # csv << [
+    #   'Code',
+    #   'Design',
+    #   'BodyStyle',
+    #   'Color',
+    #   'Size',
+    #   'Quantity'
+    # ]
     
     inventory = Nokogiri::XML(File.open(filename))
     inventory.xpath('//Product').each do |product_xml|
@@ -24,42 +24,42 @@ class InventoryUpdate
       next unless product_color
       full_og_code =~ /\-(.*)/
       size = Size.translation $1, product_color
-      puts size.name
+      # puts size.name
       quantity = product_xml.xpath('Quantity').first.content
       
       if size
         garment = product_color.garments.of_size(size.id).of_color(product_color.color_id).first
         if garment
           puts "updating inventory of #{garment.id} #{garment.name} to #{quantity}"
-          garment.set_inventory quantity
+          garment.set_inventory quantity.to_i
         
-          csv << [
-            garment.product_color.og_code,
-            garment.design.name,
-            garment.body_style.name,
-            garment.color.name,
-            garment.size.name,
-            quantity
-          ]
+          # csv << [
+          #   garment.product_color.og_code,
+          #   garment.design.name,
+          #   garment.body_style.name,
+          #   garment.color.name,
+          #   garment.size.name,
+          #   quantity
+          # ]
         elsif garment = product_color.garments.first
           puts "updating inventory of #{garment.id} #{garment.name} to #{quantity}"
-          garment.set_inventory quantity
+          garment.set_inventory quantity.to_i
           
-          csv << [
-            product_color_code,
-            garment.design.name,
-            garment.body_style.name,
-            garment.color.name,
-            garment.size.name,
-            quantity
-          ]
+          # csv << [
+          #   product_color_code,
+          #   garment.design.name,
+          #   garment.body_style.name,
+          #   garment.color.name,
+          #   garment.size.name,
+          #   quantity
+          # ]
         else
           puts "garment not found: #{product_xml.xpath('ProductName').first.content}"
         end
       end
     end
-    csv.close
+    # csv.close
     
-    ReportMailer.inventory_report(ENV['INVENTORY_REPORT_RECIPIENT_EMAIL'], File.join(Rails.root, 'tmp', 'current_inventory.csv'))
+    # ReportMailer.inventory_report(ENV['INVENTORY_REPORT_RECIPIENT_EMAIL'], File.join(Rails.root, 'tmp', 'current_inventory.csv'))
   end
 end
