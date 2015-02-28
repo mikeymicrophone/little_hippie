1c0f1a76e4f3d7ee4918bff65ea700dfda2286b0
Mike Schwab <mike.schwab@gmail.com>
2015-02-28 12:02:40 -0500

    began filtered navigation system

--- app/helpers/body_styles_helper.rb
+++ app/helpers/body_styles_helper.rb
@@ -1,2 +1,12 @@
 module BodyStylesHelper
+  def filtered_navigation_system
+    content_tag :nav, :id => 'filtered_navigation' do
+      Category.age_group.map do |age|
+        content_tag :div, :class => 'age_group' do
+          check_box_tag(age) +
+          age.name
+        end
+      end.join.html_safe
+    end
+  end
 end
