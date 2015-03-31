7238085e4388c5554d53641bafcad80fa5f26cb9
Mike Schwab <mike.schwab@gmail.com>
2015-03-30 20:57:45 -0400

    better quality images on category page

--- app/helpers/product_colors_helper.rb
+++ app/helpers/product_colors_helper.rb
@@ -1,8 +1,8 @@
 module ProductColorsHelper
-  def product_display_box product_color
+  def product_display_box product_color, size = :product_box
     div_for product_color.product, :class => "stack product_color_#{product_color.id}", :'data-product_id' => product_color.product_id, :'data-product_color_id' => product_color.id do
       like_heart_for(product_color.product) +
-	    (product_color.product.primary_image.present? ? link_to(colored_image(product_color), detail_product_path(product_color.product)) : '') +
+	    (product_color.product.primary_image.present? ? link_to(colored_image(product_color, size), detail_product_path(product_color.product)) : '') +
 	    div_for(product_color.product, :gray_label_for, :class => 'gray_label') do
         div_for(product_color.product, :price_label_for, :class => 'price_label') do
           "<span class='dollar_sign'>$</span>".html_safe +
@@ -28,7 +28,7 @@ module ProductColorsHelper
 	  end.html_safe
   end
   
-  def colored_image product_color
-    image_tag(product_color.product.primary_image(:product_box), :style => "background-color:##{product_color.color.css_hex_code}")
+  def colored_image product_color, size = :product_box
+    image_tag(product_color.product.primary_image(size), :style => "background-color:##{product_color.color.css_hex_code}")
   end
 end
