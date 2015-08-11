de29c544c03231d661f825751a2c12607a024b17
Mike Schwab <mike.schwab@gmail.com>
2015-08-11 02:43:29 -0400

    preparation for big carousels

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -43,4 +43,44 @@ module ApplicationHelper
   def detail_banner_url banner
     display_banner_url banner
   end
+  
+  def top_level_browser
+    content_tag(:div, :id => "shop_by_color_title") do
+      link_to('Browse by Color', browse_colors_path)
+    end +
+    content_tag(:div, :id => "shop_by_color") do
+    end +
+    content_tag(:div, :id => "shop_by_style_title") do
+      link_to('Browse by Style', browse_body_styles_path)
+    end +
+    content_tag(:div, :id => "shop_by_style") do
+      BodyStyle.active.map do |body_style|
+        link_to detail_body_style_path(body_style) do
+          div_for(body_style, :class => 'body_style_list similar_item') do
+            image_tag(body_style.image.url(:product_box))
+          end
+        end
+      end.join.html_safe
+    end +
+    content_tag(:div, :id => "shop_by_design_title") do
+      link_to('Browse by Design', browse_designs_path)
+    end +
+    content_tag(:div, :id => "shop_by_design") do
+      Design.featured.map do |design|
+        link_to detail_design_path(design) do
+          div_for(design, :class => 'design_list similar_item') do
+            image_tag(design.art.url(:product_box))
+          end
+        end
+      end.join.html_safe
+    end +
+    content_tag(:div, :id => "browse_sale_items_title") do
+     link_to('Sale Items', browse_sales_path)
+    end +
+    content_tag(:div, :id => "browse_sale_items") do
+      Sale.current.map(&:sale_inclusions).flatten.map(&:product_colors).flatten.uniq.map do |product_color|
+        product_display_box product_color
+      end.join.html_safe
+    end.html_safe
+  end
 end
