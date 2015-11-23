6e5d6a179201da2f58843537b40e0730bec4622b
Mike Schwab <mike.schwab@gmail.com>
2015-11-23 17:16:26 -0500

    corrected update_column syntax

--- db/migrate/20151123210626_submission_date_of_wholesale_order.rb
+++ db/migrate/20151123210626_submission_date_of_wholesale_order.rb
@@ -2,7 +2,7 @@ class SubmissionDateOfWholesaleOrder < ActiveRecord::Migration
   def up
     add_column :wholesale_orders, :submission_date, :datetime
     
-    WholesaleOrder.submitted.each { |wholesale_order| wholesale_order.update_column :submission_date => wholesale_order.updated_at }
+    WholesaleOrder.submitted.each { |wholesale_order| wholesale_order.update_column :submission_date, wholesale_order.updated_at }
   end
 
   def down
