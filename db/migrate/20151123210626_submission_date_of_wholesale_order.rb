a3c6b6c0ff5095b3717d8afef3966c559539614c
Mike Schwab <mike.schwab@gmail.com>
2015-11-23 16:45:20 -0500

    try stampless update to preserve timestamps

--- db/migrate/20151123210626_submission_date_of_wholesale_order.rb
+++ db/migrate/20151123210626_submission_date_of_wholesale_order.rb
@@ -2,7 +2,7 @@ class SubmissionDateOfWholesaleOrder < ActiveRecord::Migration
   def up
     add_column :wholesale_orders, :submission_date, :datetime
     
-    WholesaleOrder.submitted.each { |wholesale_order| wholesale_order.submission_date = wholesale_order.updated_at; wholesale_order.save }
+    WholesaleOrder.submitted.each { |wholesale_order| wholesale_order.update_column :submission_date => wholesale_order.updated_at }
   end
 
   def down
