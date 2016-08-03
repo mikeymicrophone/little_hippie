f8213c9641f4ccc13859453ceaf6e9f02b6080d6
Mike Schwab <mike.schwab@gmail.com>
2016-08-03 01:23:51 -0400

    updated index definition for searching

--- app/indices/product_color_index.rb
+++ app/indices/product_color_index.rb
@@ -1,8 +1,8 @@
 ThinkingSphinx::Index.define :product_color, :with => :active_record do
-  indexes design.name
-  indexes design.number
+  indexes design.name, :as => :design_name
+  indexes design.number, :as => :design_number
   indexes body_style.name, :as => :body_style
-  indexes body_style.code
+  indexes body_style.code, :as => :body_style_code
   indexes color.name, :as => :color
   indexes color.code, :as => :color_code
   indexes product.code, :as => :product_code, :sortable => true
--- app/indices/product_index.rb
+++ app/indices/product_index.rb
@@ -1,6 +1,6 @@
 ThinkingSphinx::Index.define :product, :with => :active_record do
-  indexes design.name
-  indexes design.number
+  indexes design.name, :as => :design_name
+  indexes design.number, :as => :design_number
   indexes body_style.name, :as => :body_style
   indexes body_style.code, :as => :body_style_code
   indexes code
