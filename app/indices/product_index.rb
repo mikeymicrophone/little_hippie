44952a00132ce73e03f9b299bf30bd93a61b9486
Mike Schwab <mike.schwab@gmail.com>
2016-08-03 01:06:19 -0400

    fields in an index have unique names

--- app/indices/product_index.rb
+++ app/indices/product_index.rb
@@ -1,7 +1,7 @@
 ThinkingSphinx::Index.define :product, :with => :active_record do
   indexes design.name
   indexes design.number
-  indexes body_style.name
-  indexes body_style.code
+  indexes body_style.name, :as => :body_style
+  indexes body_style.code, :as => :body_style_code
   indexes code
 end
