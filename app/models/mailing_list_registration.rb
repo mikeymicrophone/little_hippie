eb7157e20451507bdfd2339c7074d6d43f1af8c5
Mike Schwab <mike.schwab@gmail.com>
2016-09-06 19:33:10 -0400

    sync mailing list to mailchimp

--- app/models/mailing_list_registration.rb
+++ app/models/mailing_list_registration.rb
@@ -3,6 +3,7 @@ class MailingListRegistration < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name, :street, :street2, :city, :state, :zip, :festival, :referral_id
   validates_uniqueness_of :email, :case_sensitive => false
   before_save :downcase_email
+  after_save :send_to_mailchimp
   
   def downcase_email
     self.email = email.downcase
@@ -14,6 +15,15 @@ class MailingListRegistration < ActiveRecord::Base
       all.each do |product|
         csv << product.attributes.values_at(*column_names)
       end
+    end
+  end
+  
+  def send_to_mailchimp
+    begin
+      gibbon = Gibbon::Request.new :api_key => ENV['MAILCHIMP_API_KEY']
+      mail_list = gibbon.lists(ENV['MAILCHIMP_LIST_ID']).members.create :body => {:email_address => email, :status => 'subscribed', merge_fields: {FNAME: first_name, LNAME: last_name}}
+    rescue Gibbon::MailChimpError => mail_error
+      Rails.logger.info mail_error
     end
   end
 end
